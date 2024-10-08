f2b0ee90c61ce5ff5142044306403d589ef44ba2
Patrick Hund <patrick.hund@adevinta.com>
2024-10-08 12:23:07 +0200

    chore: remove unnecessary components

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
 export const ADDON_ID = "my-addon";
 export const TOOL_ID = `${ADDON_ID}/tool`;
-export const PANEL_ID = `${ADDON_ID}/panel`;
 export const TAB_ID = `${ADDON_ID}/tab`;
 export const KEY = `my-addon`;
 
